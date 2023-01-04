ab334b79ce268db66defd0c5a64f8bc058a95870
Matt Xiong <mattxiong_14@hotmail.com>
2023-01-04 10:47:47 -0600

    added querySelector for all of the buttons.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // first we need to set the inital value to zero
 let count = 0;
 // we can now setup functionality for our buttons on HTML page
-// we use #value in our first querySelector because we want select the first element with the id of "value"
+// we use #value in our first querySelector because we want to select the first element with the id of "value"
+// we use .btn in our second querySelctor because we want to select all of the classes that have ".btn"
 const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
