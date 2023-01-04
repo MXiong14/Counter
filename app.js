51dd8a48a28d721495377b042ea6f68c211a1b58
Matt Xiong <mattxiong_14@hotmail.com>
2023-01-04 10:43:10 -0600

    adding a querySelector to get the value of zero

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // first we need to set the inital value to zero
 let count = 0;
+// we can now setup functionality for our buttons on HTML page
+// we use #value in our first querySelector because we want select the first element with the id of "value"
+const value = document.querySelector("#value");
 
